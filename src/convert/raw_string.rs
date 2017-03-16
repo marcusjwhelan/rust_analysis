a714e38bf9f9d09e48c5956f3dcda5ad821160ff
marcusjwhelan <marcus.j.whelan@gmail.com>
2017-03-16 10:34:04 -0500

    can now convert raw csv string to a Matrix of String types

--- src/convert/raw_string.rs
+++ src/convert/raw_string.rs
@@ -1,11 +1,26 @@
 
-
 /// Struct declaring a matrix as its content
 pub struct Matrix {
-  matrix: Vec<Vec<String>>
+  pub matrix: Vec<Vec<String>>
 }
 
-/// Convert a String into a Matrix;
-pub fn to_matrix(raw_string: String) {
-  println!("{}", raw_string);
+impl Matrix {
+  pub fn new(data:Vec<Vec<String>>) -> Matrix {
+    Matrix {
+      matrix: data
+    }
+  }
+}
+
+/// Convert a csv String into a Matrix;
+pub fn to_matrix(raw_string: &String) -> Matrix {
+  
+  let twod_matrix = raw_string.lines()
+    .map(|s| s.trim()
+              .split(',')
+              .map(String::from)
+              .collect::<Vec<String>>())
+    .collect::<Vec<Vec<String>>>();
+  
+  Matrix::new(twod_matrix)
 }
